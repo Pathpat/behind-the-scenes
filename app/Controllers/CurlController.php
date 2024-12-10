4bacf3a73459558275b668d6b57dff3a05078e76
Pathpat <xristospatakias@gmail.com>
2024-12-10 15:49:01 +0200

    Replaced cURL-based HTTP requests with GuzzleHTTP client for improved performance and better error handling.

--- app/Controllers/CurlController.php
+++ app/Controllers/CurlController.php
@@ -5,33 +5,24 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Attributes\Get;
+use App\Contracts\EmailValidationInterface;
+use App\Services\Emailable\EmailValidationService;
 
 class CurlController
 {
+    public function __construct(private EmailValidationInterface $emailValidationService)
+    {
+    }
+
     #[Get(path: '/curl')]
     public function index()
     {
-        $handle = curl_init();
-        $apiKey = $_ENV['EMAILABLE_API_KEY'];
         $email = $_ENV['EMAIL'];
+       $result = $this->emailValidationService->verify($email);
 
-        $params = [
-            'api_key' => $apiKey,
-            'email' => $email,
-        ];
-
-        $url = 'https://api.emailable.com/v1/verify?' . http_build_query($params);
-
-        curl_setopt($handle, CURLOPT_URL, $url);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-
-        $content = curl_exec($handle);
-
-        if ($content !== false) {
-            $data = json_decode($content, true);
-            echo '<pre>';
-            print_r($data);
-        }
+        echo '<pre>';
+        print_r($result);
+        echo '</pre>';
     }
 
 }
