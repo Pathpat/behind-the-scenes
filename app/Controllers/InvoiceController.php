f5a8e89b3dbd2da083780f994d1d247604682fad
Pathpat <xristospatakias@gmail.com>
2024-12-11 15:19:32 +0200

    Migrate to Twig template engine

--- app/Controllers/InvoiceController.php
+++ app/Controllers/InvoiceController.php
@@ -8,32 +8,28 @@ use App\Attributes\Get;
 use App\Enums\InvoiceStatus;
 use App\Models\Invoice;
 use App\View;
-use Carbon\Carbon;
-use Symfony\Component\Mailer\MailerInterface;
-
-use function Symfony\Component\Clock\now;
+use Twig\Environment as Twig;
 
 class InvoiceController
 {
-    #[Get(path: '/invoices')]
-    public function index(): View
+    public function __construct(private Twig $twig)
     {
-        $invoices = Invoice::query()->where('status', InvoiceStatus::Paid)->get(
-        )->toArray();
-
-        return View::make('invoices/index', ['invoices' => $invoices]);
     }
 
-    #[Get(path: '/invoice/new')]
-    public function create()
+    #[Get(path: '/invoices')]
+    public function index(): string
     {
-        $invoice = new Invoice();
-        $invoice->amount = 50;
-        $invoice->status = InvoiceStatus::Pending;
-        $invoice->invoice_number = 'Invoice 222';
-        $invoice->due_date = (new Carbon())->addDay();
-        $invoice->save();
+        $invoices = Invoice::query()
+            ->where('status', InvoiceStatus::Paid)
+            ->get()
+            ->map(fn(Invoice $invoice) => [
+                'invoiceNumber' => $invoice->invoice_number,
+                'amount'        => $invoice->amount,
+                'status'        => $invoice->status->toString(),
+                'statusColor'   => $invoice->status->color()->getClass(),
+                'dueDate'       => $invoice->due_date->toDateTimeString(),
+            ]);
 
-            echo $invoice->id . ', '. $invoice->due_date;
+        return $this->twig->render('invoices/index.twig', ['invoices' => $invoices]);
     }
 }
